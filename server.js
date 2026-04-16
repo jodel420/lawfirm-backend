0d78f876e977cfea59fe19553b242819671b3d75
Jodel_Villena <76416533+jodel420@users.noreply.github.com>
2026-04-16 21:56:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const { XMLParser } = require('fast-xml-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://anicetalawfirm.vercel.app']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
